30b8816c8c1366f1252612995b004ecbe6010a13
fabiokenji <fk_nishimura@hotmail.com>
2015-03-14 00:58:42 -0700

    Adding the solution from VS to GitHub

---
+++ WebApplication2/WebForm1.aspx
@@ -0,0 +1,105 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication2.WebForm1" ClientIDMode="Static" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #btnStart {
+            color: #2A4F5E;
+            font-family: Verdana;
+            font-size: 18px;
+            width: 200px;
+        }
+
+        #btnStart:hover {
+                cursor: pointer;
+            }
+
+        .checkersBoardCell {
+            font-family: Verdana;
+            font-size: 18px;
+            color: white;
+            text-align: center;
+            vertical-align: central;
+            text-decoration: none;
+        }
+
+        .lblPlayerOneNameTop {
+            font-family: Verdana;
+            font-size: 14px;
+            color: blueviolet;
+        }
+
+        .lblPlayerTwoNameTop {
+            font-family: Verdana;
+            font-size: 14px;
+            color: darkcyan;
+        }
+
+        .lblPlayerBottom {
+            font-family: Verdana;
+            font-size: 14px;
+        }
+
+        .txtPlayerBottom {
+            font-family: Verdana;
+            font-size: 14px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ScriptManager ID="ScriptManager1" runat="server">
+            </asp:ScriptManager>
+            <asp:UpdatePanel ID="pnlMain" runat="server">
+                <ContentTemplate>
+                    <asp:Table runat="server" ID="tblHeader">
+                        <asp:TableRow>
+                            <asp:TableCell ColumnSpan="4" Width="100px" Height="30px">
+                                <asp:Label ID="lblPlayerOne" runat="server" CssClass="lblPlayerOneNameTop"></asp:Label>
+                            </asp:TableCell>
+                            <asp:TableCell ColumnSpan="4" Width="100px" Height="30px">
+                                <asp:Label ID="lblPlayerTwo" runat="server" CssClass="lblPlayerTwoNameTop"></asp:Label>
+                            </asp:TableCell>
+                        </asp:TableRow>
+                    </asp:Table>
+                    <asp:UpdatePanel runat="server" ID="pnlBody" UpdateMode="Conditional">
+                        <ContentTemplate>
+                            <asp:Table runat="server" ID="tblBody">
+                            </asp:Table>
+                        </ContentTemplate>
+                    </asp:UpdatePanel>
+                    <asp:Table runat="server" ID="tblFooter">
+                        <asp:TableRow>
+                            <asp:TableCell>
+                                <asp:Label ID="lblPlayerOneNameBottom" Text="Player One: " runat="server" CssClass="lblPlayerBottom"></asp:Label>
+                                <asp:TextBox ID="txtPlayerOneName" runat="server" CssClass="txtPlayerBottom"></asp:TextBox>
+                            </asp:TableCell>
+                            <asp:TableCell>
+                                <asp:Label ID="lblPlayerTwoNameBottom" Text="Player Two: " runat="server" CssClass="lblPlayerBottom"></asp:Label>
+                                <asp:TextBox ID="txtPlayerTwoName" runat="server" CssClass="txtPlayerBottom"></asp:TextBox>
+                            </asp:TableCell>
+                        </asp:TableRow>
+                        <asp:TableRow>
+                            <asp:TableCell ColumnSpan="4" HorizontalAlign="Right" Height="50px">
+                                <asp:Button runat="server" ID="btnStart" Text="Start" OnClick="btnStart_Click" />
+                            </asp:TableCell>
+                        </asp:TableRow>
+                        <asp:TableRow>
+                            <asp:TableCell>
+                                <asp:Label ID="lblError" runat="server"></asp:Label>
+                            </asp:TableCell>
+                        </asp:TableRow>
+                    </asp:Table>
+                </ContentTemplate>
+                <Triggers>
+                    <asp:AsyncPostBackTrigger ControlID="btnStart" EventName="Click" />
+                </Triggers>
+            </asp:UpdatePanel>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication2/WebForm1.aspx.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                //Session created to count the number of clicks on the checker board.
+                Session["roundCount"] = 1;
+            }
+
+            //Calling function to Create Checkers Board.
+            Create_Checkers_Board();
+        }
+
+        //This function creates the Checkers Board.
+        //tblBody is going to receive this board.
+        protected void Create_Checkers_Board()
+        {
+            try
+            {
+                Table tbl = new Table();
+                tbl = (Table)Page.FindControl("tblBody");
+
+                TableRow row;
+                TableCell cell;
+
+                //This LinkButton will receive the number of the round.
+                LinkButton lnk;
+
+                //Variable to control the creation of the Board.
+                Int32 line = 0;
+                Int32 column = 0;
+
+                for (line = 0; line < 8; line++)
+                {
+                    row = new TableRow();
+
+                    for (column = 0; column < 8; column++)
+                    {
+                        lnk = new LinkButton();
+                        lnk.ID = "lnk" + line.ToString() + column.ToString();
+                        lnk.Click += lnk_Click;
+                        lnk.Height = 35;
+                        lnk.Width = 35;
+                        lnk.CssClass = "checkersBoardCell";
+
+                        cell = new TableCell();
+                        cell.Height = 40;
+                        cell.Width = 40;
+
+                        //Color
+                        if (line % 2 == 0)
+                        {
+                            if (column % 2 == 0)
+                                cell.BackColor = System.Drawing.Color.Black;
+                            else
+                                cell.BackColor = System.Drawing.Color.Red;
+                        }
+                        else
+                        {
+                            if (column % 2 == 0)
+                                cell.BackColor = System.Drawing.Color.Red;
+                            else
+                                cell.BackColor = System.Drawing.Color.Black;
+                        }
+
+                        cell.Controls.Add(lnk);
+                        row.Controls.Add(cell);
+                    }
+
+                    tbl.Controls.Add(row);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                lblError.Text = "Function Create_Checkers_Board() :" + ex.Message;
+            }
+
+
+        }
+
+        //Function to delete the current checkers board
+        protected void Delete_Checkers_Board()
+        {
+            try
+            {
+                Table tbl = new Table();
+                tbl = (Table)Page.FindControl("tblBody");
+
+                Int32 i = 0;
+
+                for (i = tbl.Rows.Count - 1; i >= 0; i--)
+                {
+                    tbl.Rows.RemoveAt(i);
+                }
+            }
+            catch (Exception ex)
+            {
+                lblError.Text = "Function Delete_Checkers_Board() :" + ex.Message;
+            }
+
+        }
+
+        //OnClick event btnStart
+        //It will set the Labels at the top of the page using the contents of the TextBoxes
+        //Delete and Create the Checkers Board.
+        //Set the Session roundCount = 1
+        protected void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lblPlayerOne.Text = txtPlayerOneName.Text;
+                lblPlayerTwo.Text = txtPlayerTwoName.Text;
+
+                txtPlayerOneName.Text = "";
+                txtPlayerTwoName.Text = "";
+
+                Delete_Checkers_Board();
+                Create_Checkers_Board();
+                Session["roundCount"] = 1;
+
+            }
+            catch (Exception ex)
+            {
+
+                lblError.Text = "Function btnStart_Click() :" + ex.Message; ;
+            }
+
+        }
+
+        //Every cell on the Checkers Board is a LinkButton
+        //Find the LinkButton
+        //Get value from the Session roundCount
+        //Set text to the LinkButton
+        //Update Session roundCount
+        protected void lnk_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                LinkButton lnk = ((LinkButton)sender);
+                Int32 vl = Convert.ToInt32(Session["roundCount"].ToString());
+
+                if (lnk.Text == "")
+                {
+                    lnk.Text = vl.ToString();
+                }
+                else
+                {
+                    lnk.Text = "";
+                }
+
+                vl++;
+                Session["roundCount"] = vl;
+            }
+            catch (Exception ex)
+            {
+
+                lblError.Text = "Function lnk_Click() :" + ex.Message; ;
+            }
+
+        }
+    }
+}
